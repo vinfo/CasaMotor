a463ed9af9695c7b940be0aac95602c48738262b
VICTOR VALENCIA <victor.valencia@gmail.com>
2015-08-27 15:58:13 -0500

    Cam

--- www/js/index.js
+++ www/js/index.js
@@ -8,15 +8,6 @@ var app = {
     },
     onDeviceReady: function() {
         //scanear();
-        navigator.customCamera.getPicture(filename, function success(fileUri) {
-            alert("File location: " + fileUri);
-        }, function failure(error) {
-            alert(error);
-        }, {
-            quality: 80,
-            targetWidth: 120,
-            targetHeight: 120
-        });        
     }   
 };
 $( document ).ready(function() {
--- www/js/internal.js
+++ www/js/internal.js
@@ -45,12 +45,16 @@ function scanear(){
     );	
 }
 function photo(){
-  navigator.camera.getPicture(onSuccess, onFail, { quality: 50 });
+  navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL
+  });
 }
-function onSuccess(imageURI) {
-  $(".img").attr(imageURI);
+
+function onSuccess(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
 }
 
 function onFail(message) {
-  alert("Problemas inicializando camara" + message);
+    alert('Failed because: ' + message);
 }
