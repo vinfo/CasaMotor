27736f938781a0333ed83e5055aefb98ebc5a0f0
VICTOR VALENCIA <victor.valencia@gmail.com>
2015-08-27 16:02:53 -0500

    cambios

--- www/js/index.js
+++ www/js/index.js
@@ -7,9 +7,20 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
-        //scanear();
+      navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+          destinationType: Camera.DestinationType.DATA_URL
+      });
     }   
 };
+
+function onSuccess(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
 $( document ).ready(function() {
     $( document ).ajaxStart(function() {
       $(".load").show();
