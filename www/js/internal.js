4d9c8f0140228b94cecef1be9f7c154d06846977
VICTOR VALENCIA <victor.valencia@gmail.com>
2015-08-31 16:44:53 -0500

    Plugin Only

--- www/js/internal.js
+++ www/js/internal.js
@@ -46,20 +46,20 @@
 
   function imprimir(){
     alert("imprimir");
-            // try 
-            // {
-            //     cordova.plugins.bixolonPrint.getStatus(function() 
-            //         {
-            //             var connected = true; //my own local variable
-            //             alert('Conectada');
-            //         },function() 
-            //         {
-            //             var connected = false; //my own local variable
-            //             alert('Fallo al conectar');
-            //         },false);
-            // }
-            // catch (err) 
-            // {   
-            //     alert("Error "+err);
-            // }
+    try 
+    {
+      cordova.plugins.bixolonPrint.getStatus(function() 
+      {
+                        var connected = true; //my own local variable
+                        alert('Conectada');
+                      },function() 
+                      {
+                        var connected = false; //my own local variable
+                        alert('Fallo al conectar');
+                      },false);
+    }
+    catch (err) 
+    {   
+      alert("Error "+err);
+    }
   }
