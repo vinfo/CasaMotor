67517ea283ce2b0f329615f27f0c1d2959f18155
VICTOR VALENCIA <victor.valencia@gmail.com>
2015-08-31 19:17:20 -0500

    Final

---
+++ www/js/app.js
@@ -0,0 +1,44 @@
+
+var manifiestosApp = angular.module('manifiestosApp', []);
+
+manifiestosApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+    when('/Scanner', {
+      templateUrl: 'views/scanner.html',
+      controller: 'scannerController'
+    }).
+    when('/Placa', {
+      templateUrl: 'views/placa.html',
+      controller: 'placaController'
+    }).
+    when('/Impresion', {
+      templateUrl: 'views/impresion.html',
+      controller: 'impresionController'
+    }).      
+    otherwise({
+      templateUrl: 'views/intro.html'
+    });
+  }]);
+
+
+manifiestosApp.controller('scannerController', function($scope) {
+  $("#layout,#menu").removeClass("active");
+  $scope.getScan = function (type) { 
+    scanear();
+    return false;
+  }  
+});
+
+manifiestosApp.controller('placaController', function($scope) {
+  $("#layout,#menu").removeClass("active");
+});
+
+manifiestosApp.controller('impresionController', function($scope) {
+  $("#layout,#menu").removeClass("active");
+  $scope.sendPrint = function (type) { 
+    imprimir();
+    return false;
+  }
+});
+
---
+++ www/js/index.js
@@ -0,0 +1,71 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        //scanear();
+    }   
+};
+$( document ).ready(function() {
+    if(localStorage.getItem("IP")!=""){
+      $("#IP").val(localStorage.getItem("IP"));
+      $("em").html(localStorage.getItem("IP"));
+    } 
+    $(".config").click(function() {
+      $(".pass").fadeIn();
+    });
+    $(".btn_submit").click(function() {
+      var id=$("#ID_USUARIO").val();
+      var pass=$("#PASSWORD").val();      
+    if(id!=""){      
+      if(id!="" && pass!=""){
+        $.getJSON( "config.json", function( json ) {          
+          if(json.user==id && json.pass==pass){            
+            $(".divconfig").fadeIn();
+          }
+        });
+        $(".load").hide();
+      }
+      if(id!=""&&pass==""){
+        var IP= localStorage.getItem("IP");
+        var data= "action=login&ID_USUARIO="+$("#ID_USUARIO").val();
+        jQuery.ajax({
+           type: 'POST',
+           url: "http://"+IP+"/casamotor/services.php",
+           crossDomain: true,
+           data: data,
+           dataType: 'json',
+           async: false, 
+           timeout: 10000, // 10 seconds
+          beforeSend:function(){
+            $(".load").show();
+          },           
+           success: function(res) {
+             if(res.msg){
+               window.location.href = "internal.html";
+             }else{
+              $(".load").hide();
+              $(".msg").show();
+             }            
+           },
+           error:function(request, status, error){
+             alert("Sistema no disponible en estos momentos.");
+             $(".load").hide();
+           }
+         });
+      }
+    }else{
+      $("#ID_USUARIO").css("background-color","#FBD5D0");
+    }
+    });    
+    $("#IP").change(function() {
+      localStorage.setItem("IP", $("#IP").val());
+      $("#IP").val(localStorage.getItem("IP")).css("background-color","#CFC");
+      $(".pass").fadeOut(); 
+      $("#PASSWORD").val(''); 
+    });
+  });
